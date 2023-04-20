e607967166bf628856de3ca2a38a94eb2edb0b8b
nhi1016 <nhi1016@uc.cl>
2023-04-20 17:34:36 -0300

    UPDATE (Instructions.css, Instructions.jsx) new file (yarn.lock & node_modules/.yarn-integrity)

--- memory-matrix/src/game/Instructions.jsx
+++ memory-matrix/src/game/Instructions.jsx
@@ -21,44 +21,49 @@ function Instructions() {
             <li>Cada jugador tendrá una cantidad limitada de vidas.</li><br></br>
 
             <li>Dificultad. Este aspecto va a estar controlado por varios factores, tales como:</li><br></br>
+            
+            <ul className="Dificultad_Subsection">
+                <li type="circle">El tamaño del tablero, que puede ir de 4x4 hasta 16x16.</li><br></br>
 
-            <li type="circle">El tamaño del tablero, que puede ir de 4x4 hasta 16x16.</li><br></br>
+                <li type="circle">Tiempo. Según qué tan capaz un jugador se crea de resolver el juego, puede fijar un límite de tiempo máximo.</li> <br></br>
 
-            <li type="circle">Tiempo. Según qué tan capaz un jugador se crea de resolver el juego, puede fijar un límite de tiempo máximo.</li> <br></br>
-
-            <li type="circle">Tema de la partida, que puede ir de frutas, hasta ingeniería de cohetes, y cuya relación no será tan trivial, es decir, en un inicio 2 fichas tendrá la misma fruta a mostrar, en cambio, en el último nivel, las fichas pares mostrarán temas relacionados, pero no iguales, como el espacio y una estrella. </li><br></br>
+                <li type="circle">Tema de la partida, que puede ir de frutas, hasta ingeniería de cohetes, y cuya relación no será tan trivial, es decir, en un inicio 2 fichas tendrá la misma fruta a mostrar, en cambio, en el último nivel, las fichas pares mostrarán temas relacionados, pero no iguales, como el espacio y una estrella. </li><br></br>
+            </ul>
         </ul>
 
         <p>
         Bonus.
         </p>
         <ul>
-            <li> El juego tendrá un sistema de ayudas que permita al jugador terminarlo y no quedarse estancado, estos pueden ser: 
+            <li > El juego tendrá un sistema de ayudas que permita al jugador terminarlo y no quedarse estancado, estos pueden ser: </li><br></br>
+            <ul className="Bonus_Subsection">
+                <li type="circle">Transcurrido un tiempo de inactividad, un número limitado de tarjetas mostrarán pistas, que será una breve descripción de la imagen al reverso. </li><br></br>
 
-            Transcurrido un tiempo de inactividad, un número limitado de tarjetas mostrarán pistas, que será una breve descripción de la imagen al reverso. 
+                <li type="circle">La ficha se tornará transparente permitiendo vislumbrar algo del dibujo. </li><br></br>
+            </ul>
 
-            La ficha se tornará transparente permitiendo vislumbrar algo del dibujo. </li>
+            <li>Un turno consistirá en una partida, de la cual el jugador conseguirá una cantidad de puntos según los criterios especificados a continuación:  </li><br></br>
 
-            <li>Un turno consistirá en una partida, de la cual el jugador conseguirá una cantidad de puntos según los criterios especificados a continuación: 
+            <ul className="Bonus_Subsection">
+                <li type="circle">Mientras menos se demore más puntos recibirá.</li><br></br>
 
-            Mientras menos se demore más puntos recibirá. 
+                <li type="circle">Según el tamaño del tablero, más grande, más puntos. </li><br></br>
 
-            Según el tamaño del tablero, más grande, más puntos. 
+                <li type="circle">Mientras menos BONUS use más puntos recibe. </li><br></br>
 
-            Mientras menos BONUS use más puntos recibe. 
-
-            Mientras mayor dificultad, más puntos recibe. </li>
-
-            <p>Consideraciones</p>
-
-            <li>Se debe poder crear el juego, de tal manera que pueda haber múltiples jugadores jugando partidas distintas. 
-
-            Debe haber la posibilidad de un usuario espectador.
-
-            Recuperar la partida desde el punto en que quedó guardada. 
-            </li>
-
+                <li type="circle">Mientras mayor dificultad, más puntos recibe. </li><br></br>
+            
+            </ul>
         </ul>
+            <p>Consideraciones</p>
+            <ul>
+                <li>Se debe poder crear el juego, de tal manera que pueda haber múltiples jugadores jugando partidas distintas. 
+
+                Debe haber la posibilidad de un usuario espectador.
+
+                Recuperar la partida desde el punto en que quedó guardada. 
+            </li>
+            </ul>
         </>
     )
 }
