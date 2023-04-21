2bf648a39ac08d082091fdb4ea44d0d9a2825f42
nhi1016 <nhi1016@uc.cl>
2023-04-20 21:10:03 -0300

    UPDATED (App.jsx, Routing.jsx, .gitignore), CREATED (Signup.jsx, Signup.css)

--- memory-matrix/src/common/Routing.jsx
+++ memory-matrix/src/common/Routing.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
 import Instructions from '../game/Instructions'
 import UserWelcome from '../profile/UserWelcome'
+import Signup from '../game/signup'
 
 function Routing(){
     return (
@@ -11,6 +12,7 @@ function Routing(){
                 <Route path={'/instructions'} element={<Instructions/>}/>
                 <Route path={'/welcome'} element={<UserWelcome/>}/>
                 <Route path={'/'} element={<App/>}/>
+                <Route path={'/signup'} element={<Signup/>}/>
             </Routes>
         </BrowserRouter>
         </>
